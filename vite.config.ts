f49c2e8ea28dc0cf971046aa59d93542b96c255e
Anirudh S <anirudhsudheer@gmail.com>
2025-10-29 21:34:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or your framework plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  preview: {
+    allowedHosts: ['sudoanirudh.onrender.com'],
+    host: true,
+    port: process.env.PORT || 5173,
   },
-});
+})
