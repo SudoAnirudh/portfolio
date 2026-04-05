f060ac5acae91a232809aa6d9662fc61fb889e78
Anirudh S <78668573+SudoAnirudh@users.noreply.github.com>
2026-04-05 20:24:56 +0530

    chore: update site URL in robots.txt and sitemap configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://sudoanirudh.vercel.app/sitemap.xml',
+    sitemap: 'https://portfolio-blue-five-10.vercel.app/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://sudoanirudh.vercel.app/',
+      url: 'https://portfolio-blue-five-10.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
