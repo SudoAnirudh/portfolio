9a466a22f3013b37ac8240020a04470defc3618e
Anirudh S <78668573+SudoAnirudh@users.noreply.github.com>
2026-09-24 23:27:39 +0530

    chore: configure distDir to dist in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  distDir: "dist",
   turbopack: {
     root: path.resolve(__dirname),
   },
